98b5bf011f44b89a2e390cfe93e5247f9983b82f
Shahul Hammeed <53307156+shahul01@users.noreply.github.com>
2023-03-08 21:32:15 +0530

    feat: added buttons etc for functions

--- src/features/Home/_components/Intro/Form/_components/Register/Register.tsx
+++ src/features/Home/_components/Intro/Form/_components/Register/Register.tsx
@@ -6,6 +6,17 @@ interface IRegisterProps {
 
 const Register: FC<IRegisterProps> = (props) => {
 
+  const [isErrorRegister, setIsErrorRegister] = useState(false);
+
+  function handleForgotPassword() {
+    return '';
+  };
+
+  function handleLogin() {
+    return '';
+  };
+
+
   return (
     <div className={styles['register']}>
       <div className="inputs-container">
@@ -16,13 +27,23 @@ const Register: FC<IRegisterProps> = (props) => {
         <input type="password" placeholder='password'/>
       </div>
       <div className="error-banner">
-
+        {
+          isErrorRegister && (
+            <div className="error-text">
+              Incorrect username or password
+            </div>
+          )
+        }
       </div>
       <div className="buttons-container">
-
+        <p className="btn-forgot" onClick={handleForgotPassword}>
+          Forgot Password?
+        </p>
+        <button onClick={handleLogin}>
+          Login
+        </button>
       </div>
       <div className="notes">
-
       </div>
 
     </div>
